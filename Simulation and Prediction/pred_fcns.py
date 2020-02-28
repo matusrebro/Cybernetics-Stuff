466b2d31ab9027a2d909cfcf294e99229ddd10b3
matusrebro <matus.rebro@tutanota.com>
2020-02-28 16:06:19 +0100

    finished simulation function and fixed few bugs
    So far tested identification and simulation for 1x1 ARX model

--- Simulation and Prediction/pred_fcns.py
+++ Simulation and Prediction/pred_fcns.py
@@ -7,192 +7,185 @@ import numpy as np
 
 
 
-class Prediction:
+"""
+
+Model structure: example for 2x2 MIMO system:
     
-    # ARX prediction N-steps ahead
-    def arx_pred(ny, nu, h, theta, N):
+y1[k] = [ -y1[k-1]  -y2[k-1] + u1[k-1] + u2[k-1]] * Theta1
+y2[k] = [ -y1[k-1]  -y2[k-1] + u1[k-1] + u2[k-1]] * Theta2
+
+and thus:
+    
+[y1[k] y2[k]] = [ -y1[k-1]  -y2[k-1]  u1[k-1]  u2[k-1]] [Theta1 Theta2]
+
+ny : orders of all outputs to model, eg. ny = [1, 1]
+nu : orders of all inputs to model, eg. nu = [1, 1]
+h : regressor vector eg. h = [ -y1[k-1]  -y2[k-1]  u1[k-1]  u2[k-1] ]
+
+"""
+
+
+# ARX prediction N-steps ahead
+def arx_pred(ny, nu, h, theta, N):
+    
+    
+
+    
+    # check for theta vector shape
+    if len(theta.shape) == 1:
+        outputCount = 1
+    elif len(theta.shape) > 1:
+        outputCount = theta.shape[1]
+    else:
+        raise ValueError("Invalid theta shape")
+    
+    
+    if len(ny) != outputCount:
+        raise ValueError("Invalid ny vector length")
         
-        
-        """
-        
-        Model structure: example for 2x2 MIMO system:
-            
-        y1[k] = [ -y1[k-1]  -y2[k-1] + u1[k-1] + u2[k-1]] * Theta1
-        y2[k] = [ -y1[k-1]  -y2[k-1] + u1[k-1] + u2[k-1]] * Theta2
-        
-        and thus:
-            
-        [y1[k] y2[k]] = [ -y1[k-1]  -y2[k-1]  u1[k-1]  u2[k-1]] [Theta1 Theta2]
-        
-        ny : orders of all outputs to model, eg. ny = [1, 1]
-        nu : orders of all inputs to model, eg. nu = [1, 1]
-        h : regressor vector eg. h = [ -y1[k-1]  -y2[k-1]  u1[k-1]  u2[k-1] ]
-        
-        """
-        
-        # check for theta vector shape
-        if len(theta.shape) == 1:
-            outputCount = 1
-        elif len(theta.shape) > 1:
-            outputCount = theta.shape[1]
+    if np.sum(ny) + np.sum(nu) != len(h):
+        raise ValueError("Length of h vector doesnt equal to sum of orders of inputs and outputs")
+    
+    
+    nyu = np.concatenate((ny, nu)) # orders of both inputs and outputs of model
+    
+    if N>0:
+        if N==1:
+            y = np.dot(h,theta) # case of one step ahead prediction
         else:
-            raise ValueError("Invalid theta shape")
-        
-        
-        if len(ny) != outputCount:
-            raise ValueError("Invalid ny vector length")
+            y = np.zeros([N,outputCount]) # case of multiple steps ahead prediction
             
-        if np.sum(ny) + np.sum(nu) != len(h):
-            raise ValueError("Length of h vector doesnt equal to sum of orders of inputs and outputs")
+            for k in range(N):
+                # here happens all of recursion stuff for prediction
+                if k > 0:
+                    
+                    index = 0 # starting index by which we locate which part of regressor vector we can shift
+                    n_index = 0 # keeping count of the inner-most loop iteration
+                    for n in nyu:
+                        # here happens shifting in discrete time
+                        h[index : n+index] = np.roll( h[index : n+index], 1)
+                        # differentiation between inputs and outputs
+                        if n_index < outputCount:
+                            h[index] = y[k-1, n_index] # putting last predicted value to k-1 place in regressor
+                        else:
+                            h[index] = 0 # since model input are not predicted we put zero value here
+                        
+                        index = index + n
+                        n_index = n_index +1
+
+                y[k,:] = np.dot(h, theta) # one step ahead prediction
+    else:
+        raise ValueError("Prediction horizon N must be greater than zero")
+    
+    return y
+
+
+# ARX model simulation for input u
+def arx_sim(ny, nu, u, y0, theta):
+    
+    # check for theta vector shape
+    if len(theta.shape) == 1:
+        outputCount = 1
+    elif len(theta.shape) > 1:
+        outputCount = theta.shape[1]
+    else:
+        raise ValueError("Invalid theta shape")
+    
+    if len(u.shape) == 1:
+        inputCount = 1
+    elif len(u.shape) > 1:
+        inputCount = u.shape[1]
+    else:
+        raise ValueError("Invalid input array shape")     
+    
+    nyu = np.concatenate((ny, nu))
+    
+    y=np.zeros([u.shape[0], outputCount])
+    y[0, :]=y0
+
+    h = np.zeros(np.sum(nyu))
+    
+    # putting initial condition to regressor vector
+    # index = 0 
+    # n_index = 0
+    # for n in nyu:
+    #     if n_index < outputCount:
+    #         h[0,index] = y[0, n_index] 
+    #     index = index + n
+    #     n_index = n_index +1
+    
+    for k in range(1,u.shape[0]):
         
-        
-        nyu = np.concatenate((ny, nu)) # orders of both inputs and outputs of model
-        
-        if N>0:
-            if N==1:
-                y = np.dot(h,theta) # case of one step ahead prediction
+
+        index = 0 # starting index by which we locate which part of regressor vector we can shift
+        n_index = 0 # keeping count of the inner-most loop iteration
+        for n in nyu:
+            # here happens shifting in discrete time
+            h[index : n+index] = np.roll( h[index : n+index], 1)
+            # differentiation between inputs and outputs
+            if n_index < outputCount:
+                h[index] = y[k-1, n_index] # putting last predicted value to k-1 place in regressor
             else:
-                y = np.zeros([N,outputCount]) # case of multiple steps ahead prediction
-                
-                for k in range(N):
-                    # here happens all of recursion stuff for prediction
-                    if k > 0:
-                        
-                        index = 0 # starting index by which we locate which part of regressor vector we can shift
-                        n_index = 0 # keeping count of the inner-most loop iteration
-                        for n in nyu:
-                            # here happens shifting in discrete time
-                            h[index : n+index] = np.roll( h[index : n+index], 1)
-                            # differentiation between inputs and outputs
-                            if n_index < outputCount:
-                                h[index] = y[k-1, n_index] # putting last predicted value to k-1 place in regressor
-                            else:
-                                h[index] = 0 # since model input are not predicted we put zero value here
-                            
-                            index = index + n
-                            n_index = n_index +1
+                h[index] = u[k-1, n_index - outputCount] # since, model input are not predicted we put zero value here
+            
+            index = index + n
+            n_index = n_index +1
 
-                    y[k,:] = np.dot(h, theta) # one step ahead prediction
-        else:
-            raise ValueError("Prediction horizon N must be greater than zero")
+        y[k,:] = np.dot(h, theta) # one step ahead prediction
         
-        return y
-
-
-class Simulation:
-
-    # ARX model simulation for input u
-    def arx_sim(ny, nu, u, y0, theta):
-        
-        # check for theta vector shape
-        if len(theta.shape) == 1:
-            outputCount = 1
-        elif len(theta.shape) > 1:
-            outputCount = theta.shape[1]
-        else:
-            raise ValueError("Invalid theta shape")
-        
-        if len(u.shape) == 1:
-            inputCount = 1
-        elif len(u.shape) > 1:
-            inputCount = u.shape[1]
-        else:
-            raise ValueError("Invalid input array shape")     
-        
-        nyu = np.concatenate((ny, nu))
-        
-        y=np.zeros([u.shape[0], outputCount])
-        y[0, :]=y0
-
-        h = np.zeros(np.sum(nyu))
-        
-        for hk in range(np.sum(ny)):
-            
-        
-        
-        for k in range(1,u.shape[0]):
-            
-            
-            
-            index = 0 # starting index by which we locate which part of regressor vector we can shift
-            n_index = 0 # keeping count of the inner-most loop iteration
-            for n in nyu:
-                # here happens shifting in discrete time
-                h[index : n+index] = np.roll( h[index : n+index], 1)
-                # differentiation between inputs and outputs
-                if n_index < outputCount:
-                    h[index] = y[k-1, n_index] # putting last predicted value to k-1 place in regressor
-                else:
-                    h[index] = 0 # since, model input are not predicted we put zero value here
-                
-                index = index + n
-                n_index = n_index +1
-
-            y[k,:] = np.dot(h, theta) # one step ahead prediction
-            
-            
-            
-            
-            
-            
-            
-            y[k]=-np.dot(yp,theta_AR) + np.dot(up,theta_X)
-            yp=np.roll(yp,1)
-            yp[0]=y[k]
-            up=np.roll(up,1)
-            up[0]=u[k]        
-        return y
+    return y
 
 
 
-class ParameterEstimation:
+
+# ARX model parameter estimation via Least-Squares method
+def arx_par_est(ny, nu, y, u):
     
-    # ARX model parameter estimation via Least-Squares method
-    def arx_par_est(ny,nu,y,u):
+    if y.shape[0] != u.shape[0]:
+        raise ValueError("Input and output array length must match")
+    
+    if len(y.shape) == 1:
+        outputCount = 1
+        y.shape = (y.shape[0], 1)
+    elif len(y.shape) > 1:
+        outputCount = y.shape[1]
+    else:
+        raise ValueError("Invalid output array shape")
+    
+    if len(u.shape) == 1:
+        inputCount = 1
+        u.shape = (u.shape[0], 1)
+    elif len(u.shape) > 1:
+        inputCount = u.shape[1]
+    else:
+        raise ValueError("Invalid input array shape")        
+    
+    if len(ny) != outputCount or len(nu) != inputCount:
+        raise ValueError("Input or output dimensions do not match the sizes of ny/nu arrays")
+    
+    
+    nyu = np.concatenate((ny, nu))
+    
+    # regressor matrix initialization
+    H=np.zeros([y.shape[0],np.sum(nyu)])
+    
+    # filling up the matrix with output and input signals
+
+    index = 0 # starting index by which we locate which part of regressor vector we can shift
+    n_index = 0 # keeping count of the inner-most loop iteration
+    for n in nyu:
+        for k in range(n):
+            if n_index < outputCount:
+                H[:,index+k] = np.roll(y[:,n_index],k+1)
+                H[0:k+1,index+k] = np.zeros(k+1)
+            else:
+                H[:,index+k] = np.roll(u[:,n_index - outputCount],k+1)
+                H[0:k+1,index+k] = np.zeros(k+1)
+                
+        index = index + n
+        n_index = n_index + 1
         
-        if y.shape[0] != u.shape[0]:
-            raise ValueError("Input and output array length must match")
-        
-        if len(y.shape) == 1:
-            outputCount = 1
-        elif len(y.shape) > 1:
-            outputCount = y.shape[1]
-        else:
-            raise ValueError("Invalid output array shape")
-        
-        if len(u.shape) == 1:
-            inputCount = 1
-        elif len(u.shape) > 1:
-            inputCount = u.shape[1]
-        else:
-            raise ValueError("Invalid input array shape")        
-        
-        if len(ny) != outputCount or len(nu) != inputCount:
-            raise ValueError("Input or output dimensions do not match the sizes of ny/nu arrays")
-        
-        
-        nyu = np.concatenate((ny, nu))
-        
-        # regressor matrix initialization
-        H=np.zeros([y.shape[0],np.sum(nyu)])
-        
-        # filling up the matrix with output and input signals
-        for k in range(H.shape[1]):
-            index = 0
-            n_index = 0
-            for n in nyu:
-                for k in range(n):
-                    if n_index < outputCount:
-                        H[:,index+k] = np.roll(y[:,n_index],k+1)
-                        H[0:k+1,index+k] = np.zeros(k+1)
-                    else:
-                        H[:,index+k] = np.roll(u[:,n_index - outputCount],k+1)
-                H[0:k+1,index+k] = np.zeros(k+1)       
-                        
-                index = index + n
-                n_index = n_index + 1
-            
-        return np.linalg.lstsq(H, y, rcond=None)[0]
+    return np.linalg.lstsq(H, y, rcond=None)[0]
 
 
 
--- Simulation and Prediction/prediction_demo.py
+++ Simulation and Prediction/prediction_demo.py
@@ -5,24 +5,23 @@ stochastic LTI systems
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pred_fcns import arx_pred, arx_par_est, arx_sim
+from pred_fcns import arx_par_est, arx_sim
 
 t=np.arange(0,10,0.05)
 
 u=2*np.sin(2*np.pi*2*t)
 y=0.5*np.sin(2*np.pi*2*t-1)
 
-na=2
-nb=2
 
-theta=arx_par_est(na, nb, y, u)
+ny = [2]
+nu = [2]
 
-theta_AR=theta[0:2]
-theta_X=theta[2:]
+theta = arx_par_est(ny, ny, y, u)
 
 
-ysim=arx_sim(u, y[0], theta_AR, theta_X)
+ysim = arx_sim(ny, nu, u, [y[0]], theta)
 
-plt.plot(t,u)
-plt.plot(t,y)
-plt.plot(t,ysim)
+plt.plot(t, u, label = 'u')
+plt.plot(t, y, label = 'y')
+plt.plot(t, ysim, label = 'ysim')
+plt.legend()
