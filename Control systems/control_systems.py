29ad32f6a53d0bbd732ada99c4398c3a388afb9e
matusrebro <matus.rebro@tutanota.com>
2020-03-04 14:51:30 +0100

    update object version of LTI system definition and simulation
    Object currently consists of basic properties (parameters and states) and methods (parameter setter and simulation)

--- Control systems/control_systems.py
+++ Control systems/control_systems.py
@@ -7,7 +7,7 @@ packaged control systems module
 
 import numpy as np
 from scipy.integrate import odeint
-from scipy.signal import tf2ss
+from scipy.signal import tf2ss, ss2tf
 
 
 def fun_lin_sys_odes(x,t,u,A,B):
@@ -17,19 +17,13 @@ def fun_lin_sys_odes(x,t,u,A,B):
         return np.squeeze(np.dot(A,x)+np.dot(B,u))
 
 def sim_lin_sys_ss(u, x0, A, B, C, D, Ts):
-    x=odeint(fun_lin_sys_odes,x0,np.linspace(0,Ts),
+    x = odeint(fun_lin_sys_odes,x0,np.linspace(0,Ts),
              args=(u,A,B), rtol=1e-5
              )[-1,:]
     if np.size(u)==1:
         y=np.dot(np.transpose(C),x) + np.squeeze(D*u)
     else:
         y=np.dot(np.transpose(C),x) + np.dot(D,u)
-    return x, y
-
-
-def sim_lin_sys_tf(u, x0, num, den, Ts):
-    A, B, C, D = tf2ss(num, den)
-    x, y = sim_lin_sys_ss(u, x0, A, B, np.transpose(C), D, Ts)
     return x, y
 
 
@@ -40,17 +34,61 @@ class lin_model:
     C = 0
     D = 0
     
-    def __init__(self, parameters):
-        A, B, C, D = parameters
-        self.A = A
-        self.B = A
-        self.C = A
-        self.D = A
-    
+    num = []
+    den = []
     
     x0 = 0
     x = 0
+    y = 0
     
-    # def simulation(self, u, Ts):
+    def __init__(self, parameters):
         
+        if len(parameters) == 4:
+            self.A, self.B, self.C, self.D = parameters
+            self.num, self.den = ss2tf(self.A, self.B, self.C, self.D)
+            
+        elif len(parameters) ==2:
+            self.num, self.den = parameters
+            self.A, self.B, self.C, self.D = tf2ss(self.num, self.den)
+
+        else:
+            raise ValueError("Invalid parameter format")
+
+    def set_parameters(self, parameters):
+    
+        if len(parameters) == 4:
+            self.A, self.B, self.C, self.D = parameters
+            self.num, self.den = ss2tf(self.A, self.B, self.C, self.D)
+            
+        elif len(parameters) ==2:
+            self.num, self.den = parameters
+            self.A, self.B, self.C, self.D = tf2ss(self.num, self.den)
+    
+        else:
+            raise ValueError("Invalid parameter format")
+            
+            
+    def simulation(self, x0, t, u):
+        self.x0 = x0
+        
+        Ts = t[1] - t[0]
+        
+        x=np.zeros([len(t), len(x0)])
+
+        if len(self.C.shape)==1:
+            output_count = 1
+        else:
+            output_count = self.C.shape[0]
+
+        y = np.zeros([len(t), output_count])
+        
+        for k in range(1, len(t)):
+            self.x0, y[k] = sim_lin_sys_ss(u[k-1,:], self.x0, self.A, self.B, self.C, self.D, Ts)
+            x[k, :] = self.x0
+            
+        self.x - x
+        self.y = y
+        
+    
+    
     
