eb6b5818bb64bbdc2ec231610f670595eb4bcd93
matusrebro <matus.rebro@tutanota.com>
2020-03-09 21:51:50 +0100

    updates regarding frequency response for state-space MIMO systems

--- Control systems/control_systems.py
+++ Control systems/control_systems.py
@@ -65,6 +65,14 @@ class lin_model:
         else:
             self.input_count = self.B.shape[1]
 
+        if self.input_count = 1:
+            self.num, self.den = ss2tf(self.A, self.B, self.C, self.D)
+        else:
+            self.num = []
+            for k in range(self.input_count):
+                numk, self.den = ss2tf(self.A, self.B, self.C, self.D, input = k)
+                self.num.append(numk)
+
     def set_parameters(self, parameters):
     
         if len(parameters) == 4:
@@ -73,7 +81,6 @@ class lin_model:
             self.B = np.asarray(self.B)
             self.C = np.asarray(self.C)
             self.D = np.asarray(self.D)
-            self.num, self.den = ss2tf(self.A, self.B, self.C, self.D)
             
         elif len(parameters) ==2:
             self.num, self.den = parameters
@@ -82,6 +89,23 @@ class lin_model:
         else:
             raise ValueError("Invalid parameter format")
             
+        if len(self.C.shape)==1:
+            self.output_count = 1
+        else:
+            self.output_count = self.C.shape[0]
+
+        if len(self.B.shape)==1:
+            self.input_count = 1
+        else:
+            self.input_count = self.B.shape[1]
+            
+        if self.input_count = 1:
+            self.num, self.den = ss2tf(self.A, self.B, self.C, self.D)
+        else:
+            self.num = []
+            for k in range(self.input_count):
+                numk, self.den = ss2tf(self.A, self.B, self.C, self.D, input = k)
+                self.num.append(numk)
             
     def simulation(self, x0, t, u):
         self.x0 = x0
@@ -104,31 +128,77 @@ class lin_model:
     
     def freq_response(self, omega, plot = True):
         jomega = 1j * omega
-        nyquist = np.polyval(self.num, jomega) / np.polyval(self.den, jomega)
-        mag = 20 * np.log10(nyquist)
-        phase = np.rad2deg(np.angle(nyquist))
-        
-        if plot:
-            plt.figure()
-            plt.title('Nyquist plot')
-            plt.plot(np.real(nyquist), np.imag(nyquist))
-            plt.xlabel(r'Re{G(j$\omega$)}')
-            plt.ylabel(r'Im{G(j$\omega$)}')
-            plt.grid()
+        if self.input_count==1 and self.output_count==1:
+            nyquist = np.polyval(self.num, jomega) / np.polyval(self.den, jomega)
+            mag = 20 * np.log10(nyquist)
+            phase = np.rad2deg(np.angle(nyquist))
             
-            plt.figure()
-            plt.title('Bode plot')
-            plt.subplot(211)
-            plt.semilogx(omega,mag)
-            plt.xlabel(r'$\omega$ [rad\s]')
-            plt.ylabel(r'Magnitude [dB]')
-            plt.grid()
-            plt.subplot(212)
-            plt.semilogx(omega,phase)
-            plt.xlabel(r'$\omega$ [rad\s]')
-            plt.ylabel(r'Phase [deg]')
-            plt.grid()
-        
+            if plot:
+                plt.figure(1)
+                plt.title('Nyquist plot')
+                plt.plot(np.real(nyquist), np.imag(nyquist))
+                plt.xlabel(r'Re{G(j$\omega$)}')
+                plt.ylabel(r'Im{G(j$\omega$)}')
+                plt.grid()
+                plt.tight_layout()
+                
+                plt.figure(2)
+                plt.suptitle('Bode plot')
+                plt.subplot(211)
+                plt.semilogx(omega,mag)
+                plt.xlabel(r'$\omega$ [rad\s]')
+                plt.ylabel(r'Magnitude [dB]')
+                plt.grid()
+                plt.subplot(212)
+                plt.semilogx(omega,phase)
+                plt.xlabel(r'$\omega$ [rad\s]')
+                plt.ylabel(r'Phase [deg]')
+                plt.grid()
+                plt.tight_layout()
+                plt.subplots_adjust(top=0.92)
+        else:
+            nyquist = []
+            mag = []
+            phase = []
+            index = 1
+            
+            for j in range(self.output_count):
+                for k in range(self.input_count):
+                    nuquistk = np.polyval(self.num[k][j], jomega) / np.polyval(self.den, jomega)
+                    magk = 20 * np.log10(np.abs(nuquistk))
+                    phasek = np.rad2deg(np.angle(nuquistk))
+                    
+                    plt.figure(1)
+                    plt.suptitle('Nyquist plot')
+                    plt.subplot(self.output_count,self.input_count,index)
+                    plt.title('u'+str(k+1)+' -> y'+str(j+1))
+                    plt.plot(np.real(nuquistk), np.imag(nuquistk))
+                    plt.xlabel(r'Re{G(j$\omega$)}')
+                    plt.ylabel(r'Im{G(j$\omega$)}')
+                    plt.grid()
+                    plt.tight_layout()
+                    plt.subplots_adjust(top=0.92)
+                    
+                    plt.figure(index+1)
+                    plt.suptitle('Bode plot: u'+str(k+1)+' -> y'+str(j+1))
+                    plt.subplot(211)
+                    plt.semilogx(omega,magk)
+                    plt.xlabel(r'$\omega$ [rad\s]')
+                    plt.ylabel(r'Magnitude [dB]')
+                    plt.grid()
+                    plt.subplot(212)
+                    plt.semilogx(omega,phasek)
+                    plt.xlabel(r'$\omega$ [rad\s]')
+                    plt.ylabel(r'Phase [deg]')
+                    plt.grid()
+                    plt.tight_layout()
+                    plt.subplots_adjust(top=0.92)
+                    
+                    index+=1
+                    nyquist.append(nuquistk)
+                    mag.append(magk)
+                    phase.append(phasek)
+            
         return mag, phase, nyquist
     
     
--- Control systems/testing_playground.py
+++ Control systems/testing_playground.py
@@ -9,7 +9,7 @@ t  = np.arange(0, 10, 0.1)
 u = np.ones_like(t)
 
 
-num = [1]
+num = [6, 1]
 den = [1, 2, 1]
 
 model1 = lin_model([num, den])
@@ -30,43 +30,62 @@ mag, phase, nyquist = model1.freq_response(omega)
 
 
 A = [[-2, -1], [1, 0]]
-B = [[1, 2], [0, 0]]
+B = [[1], [0]]
 C = [[1, 2], [6, 2]] 
-D = [[0, 0], [0, 0]]
+D = [[0], [0]]
 
 
 
 model2 = lin_model([A, B, C, D])
 
-model2.input_count
-
+model2.num
 
 
 num = []
 
-for k in range(2):
+for k in range(model2.input_count):
     numk, den = ss2tf(A, B, C, D, input = k)
     num.append(numk)
 
+np.squeeze(num)
 
 nyquist = []
 mag = []
 phase = []
 index = 1
-for j in range(2):
-    for k in range(2):
+omega = np.logspace(-3, 3, 100)
+jomega = 1j * omega
+for j in range(model2.output_count):
+    for k in range(model2.input_count):
         nuquistk = np.polyval(num[k][j], jomega) / np.polyval(den, jomega)
-        magk = 20 * np.log10(nuquistk)
+        magk = 20 * np.log10(np.abs(nuquistk))
         phasek = np.rad2deg(np.angle(nuquistk))
         
         plt.figure(1)
         plt.suptitle('Nyquist plot')
-        plt.subplot(2,2,index)
+        plt.subplot(model2.output_count,model2.input_count,index)
+        plt.title('u'+str(k+1)+' -> y'+str(j+1))
         plt.plot(np.real(nuquistk), np.imag(nuquistk))
         plt.xlabel(r'Re{G(j$\omega$)}')
         plt.ylabel(r'Im{G(j$\omega$)}')
         plt.grid()
         plt.tight_layout()
+        plt.subplots_adjust(top=0.92)
+        
+        plt.figure(index+1)
+        plt.suptitle('Bode plot: u'+str(k+1)+' -> y'+str(j+1))
+        plt.subplot(211)
+        plt.semilogx(omega,magk)
+        plt.xlabel(r'$\omega$ [rad\s]')
+        plt.ylabel(r'Magnitude [dB]')
+        plt.grid()
+        plt.subplot(212)
+        plt.semilogx(omega,phasek)
+        plt.xlabel(r'$\omega$ [rad\s]')
+        plt.ylabel(r'Phase [deg]')
+        plt.grid()
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.92)
         
         index+=1
         nyquist.append(nuquistk)
